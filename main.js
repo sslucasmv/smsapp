792c9b50e7db094e276be386db9febd61a44fcb8
sslucasmv <lmvolpe24@gmail.com>
2024-06-18 23:38:33 -0300

    sms partes

--- main.js
+++ main.js
@@ -2,16 +2,24 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const input = document.getElementById('miInput');
     const container = document.getElementById('miDiv');
     const contador = document.getElementById('contador');
+    const partesMensaje = document.getElementById('partesMensaje');
 
     input.addEventListener('input', function() {
         // Limpiar el contenedor
         container.innerHTML = '';
         
-        // Obtener el valor del input
+        // Obtener el valor del textarea
         const value = input.value;
 
+        // Calcular el mensaje basado en el número de caracteres
+        const messageCount = Math.floor(value.length / 160) + 1;
+        
         // Actualizar el contador de caracteres
-        contador.textContent = value.length;
+
+        contador.textContent = `${value.length}`;
+        partesMensaje.textContent = `${messageCount}`;
+
+    
 
         // Crear un nuevo div o span por cada carácter
         for (let i = 0; i < value.length; i++) {
