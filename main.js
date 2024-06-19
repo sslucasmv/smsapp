662bb38b9f036f38c528a81b0b171aca9192d21e
sslucasmv <lmvolpe24@gmail.com>
2024-06-19 11:18:43 -0300

    texto default detalle

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
+
+/* PONERLE ESCRIBIR MENSAJE DONDE APARECEN, LOS NUMEROS UN MENSAJE DEFAULT */
+
+/* HACER LAS MEDIA QUERIES */
+
+/* HACER LA FUNCIONALIDAD DE CARACTERES ESPECIALES */
+
+
 document.addEventListener('DOMContentLoaded', (event) => {
     const input = document.getElementById('miInput');
     const container = document.getElementById('miDiv');
     const contador = document.getElementById('contador');
     const partesMensaje = document.getElementById('partesMensaje');
+    const mensajeDefault = document.getElementById('mensajeDefault');
 
+    
     input.addEventListener('input', function() {
+        
         // Limpiar el contenedor
-        container.innerHTML = '';
+        container.innerHTML = "";
         
         // Obtener el valor del textarea
         const value = input.value;
+
+        // evaluar si tiene algo el input para mostrar mensaje default
+        if(value.length > 0){
+            mensajeDefault.style.display = 'none';
+        } else {
+            mensajeDefault.style.display = 'block';
+        }
+
+
 
         // Calcular el mensaje basado en el número de caracteres
         const messageCount = Math.floor(value.length / 160) + 1;
