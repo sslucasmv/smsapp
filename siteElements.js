1a3cc99946fd5f5ea55e577deb12e73e00e2a331
sslucasmv <lmvolpe24@gmail.com>
2024-06-25 00:31:44 -0300

    cambios logo

--- siteElements.js
+++ siteElements.js
@@ -1,7 +1,7 @@
   // Funcionalidad del sticky header
   const header = document.getElementById('header');
   const sticky = header.offsetTop;
-  const logoText = document.getElementById('logoText')
+
 
   // Por defecto, agregar la clase text-shadow-logo al logoText
   logoText.classList.add('text-shadow-logo');
@@ -9,10 +9,10 @@
   function checkSticky() {
       if (window.pageYOffset > sticky) {
           header.classList.add('sticky');
-          logoText.classList.remove('text-shadow-logo');
+   
       } else {
           header.classList.remove('sticky');
-          logoText.classList.add('text-shadow-logo');
+     
           
         
       }
